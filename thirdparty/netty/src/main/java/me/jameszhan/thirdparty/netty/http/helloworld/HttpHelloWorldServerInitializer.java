7545e5179c73d46c6b2c55e37b575953bc7bdc7d
James Zhan <zhiqiangzhan@gmail.com>
2018-09-28 19:21:08 +0800

    Add netty http

---
+++ thirdparty/netty/src/main/java/me/jameszhan/thirdparty/netty/http/helloworld/HttpHelloWorldServerInitializer.java
@@ -0,0 +1,36 @@
+package me.jameszhan.thirdparty.netty.http.helloworld;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.http.HttpServerCodec;
+import io.netty.handler.codec.http.HttpServerExpectContinueHandler;
+import io.netty.handler.ssl.SslContext;
+
+/**
+ * Create by zhiqiangzhan@gmail.com
+ *
+ * @author James Zhan
+ * Date: 2018/9/28
+ * Time: 下午4:45
+ */
+public class HttpHelloWorldServerInitializer extends ChannelInitializer<SocketChannel> {
+
+    private final SslContext sslCtx;
+
+    public HttpHelloWorldServerInitializer(SslContext sslCtx) {
+        this.sslCtx = sslCtx;
+    }
+
+    @Override
+    public void initChannel(SocketChannel ch) {
+        ChannelPipeline p = ch.pipeline();
+        if (sslCtx != null) {
+            p.addLast(sslCtx.newHandler(ch.alloc()));
+        }
+        p.addLast(new HttpServerCodec());
+        p.addLast(new HttpServerExpectContinueHandler());
+        p.addLast(new HttpHelloWorldServerHandler());
+    }
+
+}
