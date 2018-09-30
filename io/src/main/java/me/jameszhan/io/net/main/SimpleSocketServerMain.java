cad057953e8b970e48add5a354a6cdbe88e775e3
James Zhan <zhiqiangzhan@gmail.com>
2018-10-01 00:55:22 +0800

    Finish SimpleSocketServer

---
+++ io/src/main/java/me/jameszhan/io/net/main/SimpleSocketServerMain.java
@@ -0,0 +1,43 @@
+package me.jameszhan.io.net.main;
+
+import com.google.common.io.ByteStreams;
+import me.jameszhan.io.net.select.IOUtils;
+import me.jameszhan.io.net.tcp.SimpleSocketServer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+
+/**
+ * Create by zhiqiangzhan@gmail.com
+ *
+ * @author James Zhan
+ * Date: 2018/10/1
+ * Time: 上午12:32
+ */
+public class SimpleSocketServerMain {
+    private static final Logger LOGGER = LoggerFactory.getLogger(SimpleSocketServerMain.class);
+    private static final String OK_HELLO = "HTTP/1.1 200 OK\r\n" +
+            "Content-Type: text/plain\r\n" +
+            "Content-Length: 12\r\n\r\n" +
+            "Hello World!";
+
+    public static void main(String[] args) throws Exception {
+        new SimpleSocketServer(8888, (in, out) -> {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(in, IOUtils.UTF_8));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                LOGGER.info("-> {}", line);
+                if (line.isEmpty()) {
+                    break;
+                }
+            }
+            out.write(OK_HELLO.getBytes(IOUtils.UTF_8));
+            IOUtils.close(out);
+            IOUtils.close(in);
+        }).start();
+    }
+
+}
--- io/src/main/java/me/jameszhan/io/net/tcp/SimpleSocketServer.java
+++ io/src/main/java/me/jameszhan/io/net/tcp/SimpleSocketServer.java
@@ -4,7 +4,11 @@ import me.jameszhan.io.net.select.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadPoolExecutor;
@@ -19,6 +23,7 @@ import java.util.concurrent.atomic.AtomicBoolean;
  * Time: PM11:52
  */
 public class SimpleSocketServer {
+
     private static final Logger LOGGER = LoggerFactory.getLogger(SimpleSocketServer.class);
 
     private static ServerSocket serverSocket;
@@ -58,7 +63,6 @@ public class SimpleSocketServer {
         ConnectionListener listener = new ConnectionListener(port, threadPool, protocol, active);
 
         threadPool.execute(listener);
-        listener.wait();
     }
 
     /**
@@ -85,5 +89,115 @@ public class SimpleSocketServer {
         serverSocket = socket;
     }
 
+    /**
+     * Created with IntelliJ IDEA.
+     *
+     * @author zizhi.zhzzh
+     *         Date: 16/3/9
+     *         Time: PM11:52
+     */
+    public interface IOProtocol {
+        void handle(InputStream in, OutputStream out) throws IOException;
+    }
+
+    /**
+     * Created with IntelliJ IDEA.
+     *
+     * @author zizhi.zhzzh
+     *         Date: 16/3/9
+     *         Time: PM11:58
+     */
+    private static class ConnectionHandler implements Runnable {
+
+        private Socket socket;
+        private IOProtocol protocol;
+
+        /**
+         * Will get the input and output stream from socket, and setup the reader
+         * and writer.
+         *
+         * @param socket
+         */
+        ConnectionHandler(Socket socket, IOProtocol protocol) {
+            this.socket = socket;
+            this.protocol = protocol;
+        }
+
+        /**
+         * Continuously process input and output from the socket until a termination code is sent.
+         *
+         */
+        public void run() {
+            try {
+                InputStream in = socket.getInputStream();
+                OutputStream out = socket.getOutputStream();
+                protocol.handle(in, out);
+            } catch (IOException e) {
+                LOGGER.error("Unexpected IO Error.", e);
+            }
+        }
+
+        /**
+         * Close the reader and writer first, than close off the socket.
+         *
+         */
+        public void close() {
+            IOUtils.close(socket);
+            socket = null;
+        }
+    }
+
+    /**
+     * Created with IntelliJ IDEA.
+     *
+     * @author zizhi.zhzzh
+     * Date: 16/3/9
+     * Time: PM11:59
+     */
+    private static class ConnectionListener implements Runnable {
+
+        private int port;
+
+        private Executor threadPool;
+        private ServerSocket serverSocket;
+        private AtomicBoolean active;
+
+        private IOProtocol protocol;
+
+        ConnectionListener(int port, Executor threadPool, IOProtocol protocol, AtomicBoolean active) {
+            this.port = port;
+            this.threadPool = threadPool;
+            this.protocol = protocol;
+            this.active = active;
+        }
+
+        public void run() {
+            initialize();
+            try {
+                while (active.get()) {
+                    Socket socket = serverSocket.accept();
+                    threadPool.execute(new ConnectionHandler(socket, protocol));
+                }
+                serverSocket.close();
+            } catch (IOException ex) {
+                LOGGER.info("Error accepting connection, request ignored.", ex);
+            }
+
+        }
+
+        private void initialize() {
+            try {
+                serverSocket = new ServerSocket(port);
+                SimpleSocketServer.setServerSocket(serverSocket);
+                synchronized (this) {
+                    this.notifyAll();
+                }
+                active.compareAndSet(false, true);
+            } catch (IOException ex) {
+                LOGGER.info("Failed to listen on specified port: {}.", ex);
+                active.compareAndSet(true, false);
+            }
+        }
+    }
 }
 
