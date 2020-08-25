30941969e97097db4fd15db11a152ab5b63193c9
James Zhan <zhiqiangzhan@gmail.com>
2020-08-26 01:15:02 +0800

    delete promise codes

--- pattern/promise/src/main/java/me/jameszhan/pattern/promise/standard/PromiseExecutor.java
+++
@@ -1,9 +0,0 @@
-package me.jameszhan.pattern.promise.standard;
-
-import java.util.function.Consumer;
-
-public interface PromiseExecutor<T> {
-
-    void execute(Promise<T> promise, Consumer<T> resolve, Consumer<Throwable> reject);
-
-}
