33c8afe5746bb731a51f20faa8de83e1a4f1956d
James Zhan <zhiqiangzhan@gmail.com>
2020-09-17 11:22:41 +0800

    clean the codes

--- lang/legacies/src/main/java/com/zizhizhan/legacies/thirdparty/httpclient/sample/HttpClient1.java
+++ lang/legacies/src/main/java/com/zizhizhan/legacies/thirdparty/httpclient/sample/HttpClient1.java
@@ -8,24 +8,20 @@ import org.apache.commons.httpclient.HttpStatus;
 import org.apache.commons.httpclient.methods.GetMethod;
 
 public class HttpClient1 {
-	
-	public static void main(String[] args) throws IOException
-	{	
-		HttpClient client = new HttpClient();
-		
-		HttpMethod method = new GetMethod("http://maps.google.com/maps?f=q&source=s_q&hl=zh-CN&q=hotels&sll=36.130000,-115.200000&sspn=0.320000,0.720000&ie=UTF8&radius=24.3&split=1&rq=1&ev=zo&jsv=259e&vps=8&sa=N&start=60");
-		
-		
-		
-		int status =  client.executeMethod(method);
-		
-		if(status == HttpStatus.SC_OK){
-			System.out.println(method.getResponseBodyAsString());
-		}else{
-			System.out.println(status);
-		}
-	}
-	
-	
+
+    public static void main(String[] args) throws IOException {
+        HttpClient client = new HttpClient();
+
+        HttpMethod method = new GetMethod("http://maps.google.com/maps?f=q&source=s_q&hl=zh-CN&q=hotels&sll=36.130000,-115.200000&sspn=0.320000,0.720000&ie=UTF8&radius=24.3&split=1&rq=1&ev=zo&jsv=259e&vps=8&sa=N&start=60");
+
+        int status = client.executeMethod(method);
+
+        if (status == HttpStatus.SC_OK) {
+            System.out.println(method.getResponseBodyAsString());
+        } else {
+            System.out.println(status);
+        }
+    }
+
 
 }
